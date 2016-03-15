0891716c360441a5054b5410001a64553afa5c97
Lubosky <lubos@hricak.sk>
2016-03-15 13:01:42 +0100

    Added ability to upload user avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
+  dragonfly_accessor :avatar
+
   validates :username, presence: true, length: { minimum: 4, maximum: 18 }
+  validates_property :format, of: :avatar, in: [:jpeg, :jpg, :png, :gif], case_sensitive: false
 
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable #:omniauthable
