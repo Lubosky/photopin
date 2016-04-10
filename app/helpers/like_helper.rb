7735ab62618b9d05ccf61fbc7d66a7deff7a57ff
Lubos Hricak <lubos@hricak.sk>
2016-04-10 11:24:01 +0200

    Give users ability to like posts :thumbsup:

---
+++ app/helpers/like_helper.rb
@@ -0,0 +1,24 @@
+module LikeHelper
+  def get_likes(post)
+    votes = post.votes_for.up.by_type(User)
+    users = []
+    unless votes.blank?
+      votes.voters.each do |voter|
+        users.push(link_to voter.username, 'javascript:void(0)', class: 'username')
+      end
+      users.to_sentence.html_safe + like_plural(votes)
+    end
+  end
+
+  def is_liked?(post)
+    return 'icon-heart' if current_user.voted_for? post
+    'icon-heart-empty'
+  end
+
+  private
+
+  def like_plural(votes)
+    return ' like this' if votes.count > 1
+    ' likes this'
+  end
+end
