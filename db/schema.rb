012a2eeabe216baa16161f9ff994593ee095e6fe
Lubosky <lubos@hricak.sk>
2016-04-13 14:41:05 +0200

    Add name and bio to users + run migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160409212027) do
+ActiveRecord::Schema.define(version: 20160413123830) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,8 @@ ActiveRecord::Schema.define(version: 20160409212027) do
     t.string   "username"
     t.string   "avatar_uid"
     t.string   "avatar_name"
+    t.string   "name"
+    t.text     "bio"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
